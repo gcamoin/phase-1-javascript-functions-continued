9df5553cdaad9c55e4a70812b55cdc28aa1edc4e
Garrett <gcamoin5@gmail.com>
2022-12-20 16:00:19 -0500

    commit tree 7683b1dd480c753a6fa587715d90dd1a6dc2a5cb

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(param1 = "roller-skate") {
+ return `This Saturday, I want to ${param1}!`
+}
+
+function mondayWork(param2 = "go to the office") {
+    return `This Monday, I will ${param2}.`
+}
+
+
+function wrapAdjective(string = "*") {
+    return function(param3 = "special") {
+        return `You are ${string}${param3}${string}!`
+    }
+}
+
